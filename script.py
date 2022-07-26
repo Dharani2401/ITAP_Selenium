3f2e8f1ca8449cdd97b9383b4854cf7bfd70f41a
Dharani2401 <110018092+Dharani2401@users.noreply.github.com>
2022-07-26 13:29:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from keyboard import press
 import time
 from datetime import date, datetime
 xpath_list=[]
