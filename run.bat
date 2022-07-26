e771d5d2f679e8bde317477d8eb40f84e9dfb246
Dharani2401 <110018092+Dharani2401@users.noreply.github.com>
2022-07-26 16:01:05 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 @echo off
-For /d /R %G IN (script.py) do IF EXIST %%G python json/script.py %%G
+For /d /R %G%G IN (script.py) do IF EXIST %%G python json/script.py %%G
+For /d /R %G%G IN (script.py) do IF EXIST %%G python script/script.py %%G
