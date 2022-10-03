efe3bf6c6d82084ca39e8a820fb4cabf11f63a44
Dharani2401 <110018092+Dharani2401@users.noreply.github.com>
2022-10-03 11:17:01 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,2 @@
 @echo off
-for /d /R %%G IN (script.py) do (
-IF EXIST %%G (
-echo  Test Case Started %%G
-python %%G
-echo Test Case Ended
-)
-)
+for /d /R %%G IN (script.py) do IF EXIST %%G python %%G
